7f33f5c7d92a0ef1e0f8d0776b9cf2b41430318b
ukitouchtypist <alicemargatroid.19880702@gmail.com>
2022-07-10 18:24:42 +0900

    末尾の注意書きを消さないように修正

--- src/delete_content.py
+++ src/delete_content.py
@@ -1,18 +1,21 @@
 # coding: utf-8
 import re
+from tkinter import E
 
 def main() :
     new_rows = []
     with open("anime_weekly.md", mode="r", encoding="utf_8") as f :
         article_rows = [l.strip() for l in f.readlines()]
-        include_pattern = re.compile(r"(^評価:\s[A-F]{,3}\s→(\s[A-F]{,3})?$|^評価:(\s[A-F]{,3})?$|^#{,3}\s[A-F]{,3}|^\[←前回\]|\)\s次回→$)")
-        end_pattern = re.compile(r"^## 切った$")
+        include_pattern = re.compile(r"(^評価:\s[A-F]{,3}\s→(\s[A-F]{,3})?$|^評価:(\s[A-F]{,3})?$|^#{,3}\s[A-F]{,3}|^\[←前回\]|\)\s次回→$|^次回→$|^\*\*\*$)")
+        end = "## 切った"
+        end_index = article_rows.index(end)
         for row in article_rows :
-            if end_pattern.match(row):
+            if row == end :
                 break
             if include_pattern.match(row) :
                 new_rows.append(row)
-    with open("anime_weekly.md", mode="w", encoding="utf-8") as f :
+        new_rows.extend(article_rows[end_index:])
+    with open("anime_weekly.md", mode="w", encoding="utf-8", newline="\n") as f :
         for row in new_rows :
             f.write(row + "\n")
 
