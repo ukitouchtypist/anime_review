a03d568179cd5d5d828393be4968482d79aaf3bf
ukitouchtypist <alicemargatroid.19880702@gmail.com>
2022-07-10 09:42:55 +0900

    終了パターンに^を追加

--- src/delete_content.py
+++ src/delete_content.py
@@ -6,7 +6,7 @@ def main() :
     with open("anime_weekly.md", mode="r", encoding="utf_8") as f :
         article_rows = [l.strip() for l in f.readlines()]
         include_pattern = re.compile(r"(^評価:\s[A-F]{,3}\s→\s[A-F]{,3}$|^評価:(\s[A-F]{,3})?$|^#{,3}\s[A-F]{,3}|^\[←前回\]|\)\s次回→$)")
-        end_pattern = re.compile(r"## 切った$")
+        end_pattern = re.compile(r"^## 切った$")
         for row in article_rows :
             if end_pattern.match(row):
                 break
