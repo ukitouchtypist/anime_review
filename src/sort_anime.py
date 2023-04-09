00fe80e4f02259042595390bd4d443f6c59e8b13
ukitouchtypist <alicemargatroid.19880702@gmail.com>
2023-04-10 07:21:26 +0900

    先週分の内容削除

--- src/sort_anime.py
+++ src/sort_anime.py
@@ -56,6 +56,7 @@ def extract_title(row) :
         re.compile(r"^###\s(.+?)\s第\d{,2}[話幕夜場]"),
         re.compile(r"^###\s(.+?)\s第[壱一弐二参三肆四伍五陸六漆七捌八玖九拾十陌百阡千萬万億兆京]{,2}話"),
         re.compile(r"^###\s(.+?)\sEPISODE[\\.\s]\d{,2}"),
+        re.compile(r"^###\s(.+?)\sシフト[\\.\s]\d{,2}"),
         re.compile(r"^###\s(.+?)\sLv[\\.\s]\d{,2}"),
         re.compile(r"^###\s(.+?)\s【karte\d{,2}】"),
         re.compile(r"^###\s(.+?)\sMISSION:\d{,2}"),
