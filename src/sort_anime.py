6531016613b65f5f5ad913b9b8845cbc008c4040
ukitouchtypist <alicemargatroid.19880702@gmail.com>
2023-10-13 01:54:27 +0900

    WIP

--- src/sort_anime.py
+++ src/sort_anime.py
@@ -64,6 +64,8 @@ def extract_title(row) :
         re.compile(r"^###\s(.+?)\sChapter[\\.\s]\d{,2}"),
         re.compile(r"^###\s(.+?)\sScene[\\.\s]\d{,2}"),
         re.compile(r"^###\s(.+?)\schapter[\s]\d{,2}"),
+        re.compile(r"^###\s(.+?)\sLayer[\s]\d{,2}"),
+        re.compile(r"^###\s(.+?)\sRd[\\.\s]\d{,2}"),
         re.compile(r"###\s(.+?)\s\d{,2}羽目"),
         re.compile(r"###\s(.+?)\s【すてっぷ[①-⑫]】"),
         re.compile(r"###\s(.+?)\s(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\."),
